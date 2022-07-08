667f547cd23cef790934b24d73a7e16bc2675fea
abdullah1shy <101516117+abdullah1shy@users.noreply.github.com>
2022-07-08 16:11:19 +0300

    Update music.py

--- program/music.py
+++ program/music.py
@@ -42,9 +42,9 @@ async def ytdl(format: str, link: str):
 @Client.on_message(command(["تشغيل", f"play@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
-    do = requests.get(f"https://api.telegram.org/bot5291289945:AAGUiZ_CA6drdWbr5tdl6UYpwhDrPJKZ12M/getChatMember?chat_id=@VFF35&user_id={m.from_user.id}").text
+    do = requests.get(f"https://api.telegram.org/bot5291289945:AAGUiZ_CA6drdWbr5tdl6UYpwhDrPJKZ12M/getChatMember?chat_id=@kkkt6&user_id={m.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
-        await m.reply_text("اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني  \n— — — — — — — — — — — —\n - @VFF35 ")
+        await m.reply_text("اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني  \n— — — — — — — — — — — —\n - @kkkt6 ")
     else:
         
             [
